0dfc6d390fc4161b1a9b66b60351b2f66b087c19
Lufyanto Eka Fahrezi <lufyantoekalearning@gmail.com>
2026-08-27 13:32:01 +0700

    PENGENALAN CPP

---
+++ C++/Pengenalan.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std;
+
+// Main Function
+int main() {
+    cout << "Hello World!\n"; // << Simbol penyisipan
+    cout << "Lufyanto\n";
+    cout << 400;
+    cout << "\nHasil dari 16+7=>";
+    cout << 16+7 << "\n";
+    cout << "Hallo aku sedang belajar" << "\n\n";
+    cout << "Belajar bahasa CPP \n";
+    cout << "Belajar CPP itu sangat...." << endl;
+    cout << "Menyenangkan";
+    return 0;
+}
