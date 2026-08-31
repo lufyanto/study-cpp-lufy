b9fd9e287ffbb6f4e1ca6db9ed087ab1f4eee9ee
Lufyanto Eka Fahrezi <lufyantoekalearning@gmail.com>
2026-08-31 15:09:07 +0700

    WHILE-DO-FOR-NESTEDFOR

---
+++ C++/loop.cpp
@@ -0,0 +1,216 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int perulanganManual () {
+
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+    cout << "Lufyanto Eka Fahrezi"<< endl;
+
+    string namaUserCopy100 = "Lufyanto Eka Fahrezi";
+
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+    cout << namaUserCopy100 << endl;
+
+    return 0;
+}
+
+int doWhile () {
+
+    int kuotaInternetSaya = 100;
+
+    while ( kuotaInternetSaya > 0 ){
+        cout << "Sedang Menonton Youtube ....... Sisa Kuota " << kuotaInternetSaya <<"MiB"<<endl;
+        kuotaInternetSaya --;
+    }
+
+    cout << "Kuota Internet Anda sudah Habis" << endl;
+
+    return 0;
+}
+
+int beliKuota() {
+
+    int pilihan;
+
+    do {
+    cout << "======== BELI KUOTA BY TELKOM ==========" << endl;
+    cout << "MENU PILIHAN" << endl;
+    cout << endl;
+    cout << "1. Beli Kuota" << endl;
+    cout << "0. Keluar" << endl;
+    cin >> pilihan;
+    } while (pilihan != 0);
+    
+        do {
+        cout << "======== BELI KUOTA BY TELKOM ==========" << endl;
+        cout << "MENU PILIHAN" << endl;
+        cout << endl;
+        cout << "1. Beli Pulsa" << endl;
+        cout << "0. Keluar" << endl;
+        cin >> pilihan;
+
+        } while (pilihan != 0);
+
+    
+
+    cout << "Terimakasih" << endl;
+
+
+    return 0;
+}
+
+int mengirimEmail() {
+
+    int userEmail;
+
+    cout << "----- DASHBOARD MENGIRIM EMAIL -----" << endl;
+    cout << "Masukkan jumlah user didatabase yang mau dikirimkan email : " << endl;
+    cin >> userEmail;
+
+    for ( int x = 0 ; x <= userEmail ; x++){
+        cout << "sedang mengirim email....." << endl;
+        cout << "berhasil mengirim email ke user....." << x << endl;
+    }
+
+    cout << "email berhasil dikirimkan" << endl;
+
+    return 0;
+}
+
+int nestedLoop () {
+
+    for (int baris = 1; baris <= 5 ; baris++){
+        for(int kolom =1; kolom <= 5 ; kolom++){
+            cout << "[ " << baris << " , " << kolom << " ]" << endl;
+        }
+        cout << "Putaran selesai" << endl;
+    }
+
+    return 0;
+}
+
+int main () {
+
+    // perulanganManual();
+    // doWhile();
+    // beliKuota();
+    // mengirimEmail();
+    nestedLoop();
+
+    return 0;
+}
